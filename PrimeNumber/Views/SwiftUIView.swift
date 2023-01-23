38df66209bb2cda779923785a0c972f8c50f0bc9
marina <>
2023-01-23 17:07:12 +0500

    New commit

---
+++ PrimeNumber/Views/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  PrimeNumber
+//
+//  Created by marina on 23.01.2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!!!!!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
